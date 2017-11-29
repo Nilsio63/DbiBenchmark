00fda2e1407933d771b0a15f461f9eb8a377ac61
Unknown <alina.wewering@web.de>
2017-11-29 15:59:57 +0100

    Connection DB

--- Benchmark/Benchmark/src/Benchmark.java
+++ Benchmark/Benchmark/src/Benchmark.java
@@ -7,7 +7,7 @@ public class Benchmark
 	{
 		try
 		{
-			return DriverManager.getConnection("jdbc:sqlserver://192.168.230.132;", "dbi", "dbi_pass");
+			return DriverManager.getConnection("jdbc:sqlserver://192.168.230.132;DatabaseName=Benchmark;", "dbi", "dbi_pass");
 		}
 		catch (SQLException e)
 		{
