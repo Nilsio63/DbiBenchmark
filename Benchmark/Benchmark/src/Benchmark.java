dd523bc6b5522940f3133faa2cca0a8d2b20b4aa
Leon <leonarndt@live.de>
2017-11-29 15:39:41 +0100

    Start Project

--- Benchmark/Benchmark/src/Benchmark.java
+++ Benchmark/Benchmark/src/Benchmark.java
@@ -1,10 +1,31 @@
+import java.io.*;
+import java.sql.*;
 
 public class Benchmark
 {
-	public static void main(String[] args)
+	protected static Connection getConnection() throws SQLException
 	{
-		//Test LA ist dabei!
-		// Regel 3: Der Betrag von drölf ist negativ!
+		try
+		{
+			return DriverManager.getConnection("jdbc:sqlserver://192.168.232.128;databaseName=CAP;IntegratetSecurity=true;", "dbi", "dbi_pass");
+		}
+		catch (SQLException e)
+		{
+			throw new SQLException("JDBC driver not found! " + e);
+		}
 	}
-
+	
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			Connection conn = getConnection();
+			System.out.println("Connection erfolgreich erstellt!");
+		} 
+		catch (SQLException e) 
+		{
+			System.err.println(e);
+			System.exit(1);
+		}
+	}
 }
