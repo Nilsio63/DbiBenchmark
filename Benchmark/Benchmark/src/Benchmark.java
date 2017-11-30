de17c2e2cbcee32542bd0c72d15e85f0d388b665
Nils Balke <nilsbalke@t-online.de>
2017-11-30 09:39:27 +0100

    Added db statements
    Added database statements for dropping and creating tables.

--- Benchmark/Benchmark/src/Benchmark.java
+++ Benchmark/Benchmark/src/Benchmark.java
@@ -1,31 +1,120 @@
-import java.io.*;
+import java.util.*;
 import java.sql.*;
 
 public class Benchmark
 {
-	protected static Connection getConnection() throws SQLException
-	{
-		try
-		{
-			return DriverManager.getConnection("jdbc:sqlserver://192.168.230.132;DatabaseName=Benchmark;", "dbi", "dbi_pass");
-		}
-		catch (SQLException e)
-		{
-			throw new SQLException("JDBC driver not found! " + e);
-		}
-	}
+	private static final Scanner input = new Scanner(System.in);
 	
-	public static void main(String[] args) 
-	{
-		try 
-		{
-			Connection conn = getConnection();
-			System.out.println("Connection erfolgreich erstellt!");
-		} 
-		catch (SQLException e) 
-		{
-			System.err.println(e);
-			System.exit(1);
-		}
-	}
+	private static Connection con;
+
+    public static void main(String[] args)
+    {
+        System.out.print("n: ");
+        int n = input.nextInt();
+
+        createDB(n);;
+    }
+
+    private static void createDB(int n)
+    {
+        try
+        {
+            con = getConnection();
+            
+            dropTables();
+
+            createTables();
+            
+            long timePrevious = System.currentTimeMillis();
+            
+            insertToTables(n);
+            
+            long timeSpan = System.currentTimeMillis() - timePrevious;
+            
+            System.out.println("Benötigte Zeit in ms: " + timeSpan);
+
+            con.commit();
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("SqlException: " + ex.getMessage());
+        }
+    }
+    
+    private static void dropTables()
+    	throws SQLException
+    {
+    	Statement stmt = con.createStatement();
+
+        stmt.execute("drop table branches;"
+        		+ " drop table accounts;"
+        		+ " drop table tellers;"
+        		+ " drop table history");
+    }
+    
+    private static void createTables()
+    	throws SQLException
+    {
+    	Statement x = con.createStatement();
+
+	    x.execute("create table branches" +
+	            "( branchid int not null," +
+	            " branchname char(20) not null," +
+	            " balance int not null," +
+	            " address char(72) not null," +
+	            " primary key (branchid) ); ");
+	    
+	    x.execute("create table accounts" + 
+	    		"( accid int not null," + 
+	    		" name char(20) not null," + 
+	    		" balance int not null," + 
+	    		" branchid int not null," + 
+	    		" address char(68) not null," + 
+	    		" primary key (accid)," + 
+	    		" foreign key (branchid) references branches ); ");
+	    
+	    x.execute("create table tellers" + 
+	    		"( tellerid int not null," + 
+	    		" tellername char(20) not null," + 
+	    		" balance int not null," + 
+	    		" branchid int not null," + 
+	    		" address char(68) not null," + 
+	    		" primary key (tellerid)," + 
+	    		" foreign key (branchid) references branches );");
+	    
+	    x.execute("create table history" + 
+	    		"( accid int not null," + 
+	    		" tellerid int not null," + 
+	    		" delta int not null," + 
+	    		" branchid int not null," + 
+	    		" accbalance int not null," + 
+	    		" cmmnt char(30) not null," + 
+	    		" foreign key (accid) references accounts," + 
+	    		" foreign key (tellerid) references tellers," + 
+	    		" foreign key (branchid) references branches ); ");
+    }
+    
+    private static void insertToTables(int n)
+    {
+    	
+    }
+
+    private static Connection getConnection()
+            throws SQLException
+    {
+        try
+        {
+            Connection con = DriverManager.getConnection("jdbc:sqlserver://192.168.230.132;databaseName=Benchmark;",
+                    "dbi", "dbi_pass");
+
+            con.setAutoCommit(false);
+
+            return con;
+        }
+        catch (SQLException e)
+        {
+            throw new SQLException("JDBC driver not found!" + e.getMessage());
+        }
+    }
+    
 }
